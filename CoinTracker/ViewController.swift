b209944ac8d829dade7cdb1c9998bdd263c83763
Rakhimov Aibek <rakhim.aibek@gmail.com>
2018-04-25 15:40:54 +0600

    Initial Commit

---
+++ CoinTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoinTracker
+//
+//  Created by Aibek Rakhim on 4/25/18.
+//  Copyright © 2018 Alima Moon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
